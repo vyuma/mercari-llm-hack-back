d8fdf363cc06de0303e4f02767cd421549ee33fc
nawta <39507181+nawta@users.noreply.github.com>
2024-09-27 08:17:06 +0900

    revise: db_debug

--- app/db_debug.py
+++ app/db_debug.py
@@ -1,15 +1,49 @@
 import sqlite3
 
 # SQLiteデータベースに接続
-conn = sqlite3.connect('dummy_data.db')
+conn = sqlite3.connect('regulations.db')
 cursor = conn.cursor()
 
 # テーブルのスキーマを取得
-cursor.execute("PRAGMA table_info(transactions)")
+cursor.execute("PRAGMA table_info(regulations)")
 columns = cursor.fetchall()
 
 # 各カラムの名前を表示
 for column in columns:
     print(column[1])
+
+
+# データを取得して表示
+cursor.execute("SELECT * FROM regulations LIMIT 5")
+rows = cursor.fetchall()
+
+# データを表示
+for row in rows:
+    print(row)
+
+
+
+
+
+# # テーブルのスキーマを取得
+# cursor.execute("PRAGMA table_info(transactions)")
+# columns = cursor.fetchall()
+
+# # 各カラムの名前を表示
+# for column in columns:
+#     print(column[1])
+
+
+# # データを取得して表示
+# cursor.execute("SELECT * FROM transactions LIMIT 5")
+# rows = cursor.fetchall()
+
+# # データを表示
+# for row in rows:
+#     print(row)
+
+
+
+
 
 conn.close()
